cfa5a3d336d463acc6fa7bd9df92291e48bcdac3
Yeseo Kim <YKim@lcs.on.ca>
2021-02-19 11:13:58 -0500

    Initial Commit

---
+++ DadJokeApp/DadJokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokeAppApp.swift
+//  DadJokeApp
+//
+//  Created by Yeseo Kim on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct DadJokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
